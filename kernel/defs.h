24b81bb27c35aba2f5cd348da6476ef4e7933c66
Parsa_Shabanpour <parsash1380@gmail.com>
2025-06-29 10:41:56 +0330

    Implement thread creation, joining, and sleeping functionality. Add new thread-related functions to defs.h and implement thread system calls in sysproc.c.

--- kernel/defs.h
+++ kernel/defs.h
@@ -8,6 +8,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct thread;
 
 // bio.c
 void            binit(void);
@@ -107,6 +108,11 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             trigger(void);
+void exitthread();
+void freethread(struct thread *t);
+struct thread* allocthread(uint64 start_thread, uint64 stack_address, uint64 arg);
+int jointhread(uint id);
+void sleepthread(int n, uint ticks0);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
